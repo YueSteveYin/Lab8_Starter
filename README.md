dc553c206f45e4491aa479965e06bd5eea0b3e56
YueSteveYin <yuy045@ucsd.edu>
2023-05-28 20:53:33 -0700

    final

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Lab 8 - Starter
+
+Question 1: I would choose to do it within a Github action that runs whenever code is pushed. By doing so I can test the current proccess to see f there is anything need to be change at each step and test it in the environment that it is suppose to run in instead of just test it locally. 
+
+Question 2: Would you use an end to end test to check if a function is returning the correct output? (yes/no): No, unit test would be much better for testing just an output of a function 
+
+Question 3: no, because the message feature involves a lot of user actions. End-to-end testing is a way for developers to automate test cases that involve emulating user actions from start to finish while unit test is just testing individual parts of your code, which is not enough
+
+Question 4: Yes, since texting max lenght allowed for the message application is just a small single isolated individual feature of the application, no end to end complete testing needed. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,68 @@
 // unit.test.js
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
+test('Phone number test 1 true', () => {
+    expect(functions.isPhoneNumber("123-123-1233")).toBe(true);
+});
+test('Phone number test 2 true', () => {
+    expect(functions.isPhoneNumber("051-234-1123")).toBe(true);
+});
+test('Phone number test 3 false', () => {
+    expect(functions.isPhoneNumber("123")).toBe(false);
+});
+test('Phone number test 4 false', () => {
+    expect(functions.isPhoneNumber("")).toBe(false);
+});
 
+test('isEmail test 1 true', () => {
+    expect(functions.isEmail("abc@google.com")).toBe(true);
+});
+test('isEmail test 2 true', () => {
+    expect(functions.isEmail("12345@yahoo.com")).toBe(true);
+});
+test('isEmail test 3 false', () => {
+    expect(functions.isEmail("123")).toBe(false);
+});
+test('isEmail test 4 false', () => {
+    expect(functions.isEmail("")).toBe(false);
+});
+
+test('isStrongPassword test 1 true', () => {
+    expect(functions.isStrongPassword("A168999")).toBe(true);
+});
+test('isStrongPassword test 2 true', () => {
+    expect(functions.isStrongPassword("ABC123")).toBe(true);
+});
+test('isStrongPassword test 3 false', () => {
+    expect(functions.isStrongPassword("A12")).toBe(false);
+});
+test('isStrongPasswordr test 4 false', () => {
+    expect(functions.isStrongPassword("1234")).toBe(false);
+});
+
+test('isDate test 1 true', () => {
+    expect(functions.isDate("1/2/2002")).toBe(true);
+});
+test('isDate test 2 true', () => {
+    expect(functions.isDate("11/21/2003")).toBe(true);
+});
+test('isDate test 3 false', () => {
+    expect(functions.isDate("1/3/2")).toBe(false);
+});
+test('isDate test 4 false', () => {
+    expect(functions.isDate("/1/2002")).toBe(false);
+});
+
+test('isHexColor test 1 true', () => {
+    expect(functions.isHexColor("234")).toBe(true);
+});
+test('isHexColor test 2 true', () => {
+    expect(functions.isHexColor("123")).toBe(true);
+});
+test('isHexColor test 3 false', () => {
+    expect(functions.isHexColor("12323423")).toBe(false);
+});
+test('isHexColor test 4 false', () => {
+    expect(functions.isHexColor("")).toBe(false);
+});
 // TODO - Part 2
